0fc05805a3504496b635c4dd1e23c2da5be045a9
Mufaqar <mufaqar@gmail.com>
2026-03-26 14:53:20 +0500

    TR

--- inc/woo-extra.php
+++ inc/woo-extra.php
@@ -1,1 +1,13 @@
 <?php
+
+
+add_action('init', 'remove_product_category_base');
+function remove_product_category_base() {
+    global $wp_rewrite;
+
+    // Remove product-category base
+    $wp_rewrite->extra_permastructs['product_cat']['struct'] = '/%product_cat%';
+
+    // Flush rewrite rules (do once!)
+    flush_rewrite_rules(); // Uncomment only once to flush
+}
