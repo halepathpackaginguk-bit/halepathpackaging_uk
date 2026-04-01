a0769469334127f13c6e93cbce41194d1256498f
Mufaqar <mufaqar@gmail.com>
2026-04-01 14:05:34 +0500

    live chat

--- footer.php
+++ footer.php
@@ -226,6 +226,20 @@ $instagram_feed = get_instagram_feed(); ?>
 
     });
 </script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6156ead1d326717cb6843ced/1jl44h4on';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
