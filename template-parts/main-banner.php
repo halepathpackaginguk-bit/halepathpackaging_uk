83c6d699ce65eda8f03753dc639cbf3c9fcd909e
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2026-04-02 13:15:57 +0500

    Video

--- template-parts/main-banner.php
+++ template-parts/main-banner.php
@@ -7,11 +7,13 @@ $btn_link = $home['btnlink'] ?? '';
 
 <section class="Main_slider">
     <div class="main-slider">
-        <div class="video-container">
-            <video autoplay muted loop playsinline>
-                <source src="<?php echo get_template_directory_uri(); ?>/assets/videos/video-new.mp4" type="video/mp4">
-            </video>
-
+        <div class="w-full relative md:h-[605px] overflow-hidden">
+    <iframe
+        class="absolute top-1/2 left-1/2 sm:w-[120vw] sm:h-[120vh] w-[210vw] h-[210vh] -translate-x-1/2 -translate-y-1/2"
+        src="https://www.youtube.com/embed/dfViGz8LoZE?autoplay=1&mute=1&controls=0&loop=1"
+       allow="autoplay; fullscreen"
+        frameborder="0">
+    </iframe>
             <div class="video_overlay">
                 <div class="hale_container relative z-10">
                     <div class="md:w-1/2 w-full">
