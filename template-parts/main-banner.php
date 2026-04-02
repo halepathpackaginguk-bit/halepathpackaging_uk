8b4476a9d82f67e082650a887ca947423e29d6bb
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2026-04-02 13:42:59 +0500

    Video height

--- template-parts/main-banner.php
+++ template-parts/main-banner.php
@@ -7,7 +7,7 @@ $btn_link = $home['btnlink'] ?? '';
 
 <section class="Main_slider">
     <div class="main-slider">
-        <div class="w-full relative md:h-[605px] overflow-hidden">
+        <div class="w-full relative 2xl:h-[705px] md:h-[605px] overflow-hidden">
     <iframe
         class="absolute top-1/2 left-1/2 sm:w-[120vw] sm:h-[120vh] w-[210vw] h-[210vh] -translate-x-1/2 -translate-y-1/2"
         src="https://www.youtube.com/embed/dfViGz8LoZE?autoplay=1&mute=1&controls=0&loop=1"
