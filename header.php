c5e22be99e1a38899c674bf88e96e40d3c5af5b8
Mufaqar <mufaqar@gmail.com>
2026-04-22 18:55:23 +0500

    updated tawk

--- header.php
+++ header.php
@@ -56,17 +56,7 @@
 
     gtag('config', 'AW-18062243619');
     </script>
-    <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6156ead1d326717cb6843ced/1jl44h4on';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
+  
 </head>
 <?php
 $megaMenus = [
