473dadea870181f3194ab83e4d80f10071cb8724
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2026-04-24 16:07:45 +0500

    Brands

--- template-parts/home-brands.php
+++ template-parts/home-brands.php
@@ -1,19 +1,58 @@
 <section class="pb-12 md:px-4 px-4">
-    <div class="hale_container md:p-8! p-8! shadow-[0_0_5px_0_rgba(0,0,0,0.31)] rounded-[19px]">
+    <div class="hale_container md:p-8! p-8!">
         <h2 class="h2">
-          Trusted by Global Brands. Delivered with Local Precision.
+            Sell custom playing cards online
         </h2>
-        <div class="grid md:grid-cols-5 grid-cols-3 sm:gap-7 gap-5 justify-between">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/1.png" alt="brand1" width="179"
-                height="101" />
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/2.png" alt="brand2" width="179"
-                height="101" />
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/3.png" alt="brand3" width="179"
-                height="101" />
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/4.png" alt="brand4" width="179"
-                height="101" />
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/5.png" alt="brand5" width="179"
-                height="101" />
+        <p class="sm:text-lg text-sm font-normal text-txt_Clr text-center md:w-5/6 mx-auto">
+            Seamless eCommerce integration is at the core of what we offer at halepathpackeging.co.uk. Connect your
+            online store effortlessly with leading sales channels such as Etsy, eBay, WooCommerce, Amazon, Shopify, and
+            more. Sync your products in just a few clicks, manage your listings with ease, and build a fully stocked
+            virtual catalog featuring high-quality mockups and optimized product presentations—all designed to help you
+            sell smarter and grow faster.
+        </p>
+        <div class="flex flex-wrap sm:gap-7 gap-5 justify-center mt-8">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/1.png" alt="brand1"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/2.png" alt="brand2"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/3.png" alt="brand3"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/4.png" alt="brand4"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/5.png" alt="brand5"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/6.svg" alt="brand6"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/7.svg" alt="brand7"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/8.svg" alt="brand8"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/9.svg" alt="brand9"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/10.svg" alt="brand10"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/11.svg" alt="brand11"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/12.svg" alt="brand12"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/13.svg" alt="brand13"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/brands/14.svg" alt="brand14"
+                class="object-contain object-center scale-90 hover:scale-105 transition-all ease-in-out duration-300"
+                width="200" height="101" />
         </div>
     </div>
 </section>
