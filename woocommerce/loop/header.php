c364883dd6dc5611a813ba1fad9443ba32b64fa1
Mufaqar <mufaqar@gmail.com>
2026-05-14 13:19:00 +0500

    description

--- woocommerce/loop/header.php
+++ woocommerce/loop/header.php
@@ -42,6 +42,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	 * @hooked woocommerce_taxonomy_archive_description - 10
 	 * @hooked woocommerce_product_archive_description - 10
 	 */
-	do_action( 'woocommerce_archive_description' );
+	//do_action( 'woocommerce_archive_description' );
 	?>
 </header>
