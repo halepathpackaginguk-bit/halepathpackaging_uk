d7260c8da53cb8c42c48c9822b2f2871482b9408
Mufaqar <mufaqar@gmail.com>
2026-08-18 16:20:38 +0500

    title

--- header.php
+++ header.php
@@ -19,28 +19,6 @@
     <?php if (is_search()) { ?>
     <meta name="robots" content="noindex, nofollow" />
     <?php } ?>
-    <title>
-        <?php
-        /*
-         * Print the <title> tag based on what is being viewed.
-         */
-        global $page, $paged, $post;
-
-        wp_title('|', true, 'right');
-
-        // Add the blog name.
-        bloginfo('name');
-
-        // Add the blog description for the home/front page.
-        $site_description = get_bloginfo('description', 'display');
-        if ($site_description && (is_home() || is_front_page()))
-            echo " | $site_description";
-
-        // Add a page number if necessary:
-        if ($paged >= 2 || $page >= 2)
-            echo ' | ' . sprintf(__('Page %s', 'wpv'), max($paged, $page));
-        ?>
-    </title>
     <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
